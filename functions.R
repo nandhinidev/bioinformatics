081c25824ea2f1811496128a98374798ccb68eba
nandhinidev <nandhini.guhan@gmail.com>
2019-10-10 15:45:57 -0700

    Fast code commit

--- functions.R
+++ functions.R
@@ -35,3 +35,43 @@ PatternMatch <- function(pattern,text){
   index = which(pairs %in% pattern)
   return(index-1)
 }
+
+ClumpFinding <- function(text,k,L,t){ 
+  lseq = nchar(text) 
+  interval = substring(text,seq(1,lseq-L+1),seq(L,lseq)) 
+  pattint = lapply(interval,function(x) substring(x,seq(1,L-k+1),seq(k,L))) 
+  upattint = lapply(pattint,function(x) unique(x))
+  intcount = lapply(pattint,function(x) unlist(lapply(unique(x),function(y) sum(stringr::str_count(y,paste0("^",x,"$")))))) 
+  count = lapply(intcount,function(x) which(x>=t)) 
+  clump = unique(unlist(mapply(function(x,y) x[y], x= pattint,y=count)))
+  return(noquote(clump))
+}
+
+ClumpFindingSW <- function(text,k,L,t){ 
+  lseq = nchar(text) 
+  single = unlist(strsplit(text,""))
+  pint = list()
+  interval = substring(text,1,L) 
+  pattint = substring(interval,seq(1,L-k+1),seq(k,L))
+  nocc = unlist(lapply(unique(pattint),function(x) sum(stringr::str_count(pattint,paste0("^",x,"$")))))
+  patnfreq = list(kmer=pattint,freq=nocc)
+  pint[[1]] = pattint 
+  for (i in 2:lseq){
+    newpat = paste(c(unlist(strsplit(tail(pint[[i-1]],1),""))[-1],single[i+L-1]),collapse="")
+    pint[[i]] = c(pint[[i-1]][-1],newpat)
+    if (!newpat %in% pattint) {
+      pattint = c(pattint,newpat)
+      nocc = c(nocc,1)
+      patnfreq$kmer = pattint
+      patnfreq$freq = nocc
+    }  
+    else{
+      patnfreq$kmer = pattint
+      pos = which(pattint==newpat)
+      nocc[pos] = nocc[pos] + 1
+      patnfreq$freq = nocc
+    }
+  }
+  clump = patnfreq$kmer[which(patnfreq$freq>=t)]
+  return(noquote(clump))
+}
